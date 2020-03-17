412aaaf14cf0c7e0f4b094447e50423bfa937a4f
Kaira <Kaira@KaiRa>
2020-03-17 23:48:25 +0800

    First commit

---
+++ src/fcu/iecs/oop/Homework1.java
@@ -0,0 +1,14 @@
+package fcu.iecs.oop;
+
+/*
+ * Homework 1
+ */
+
+public class Homework1 {
+
+  public static void main(String[] args) {
+    String lyrics = "Let her go! Let her go! Can't hold her back anymore";
+    System.out.print(lyrics);
+  }
+
+}
